ff2a0d1a113a30d7a60ba1cd628ba4523b1d6ea1
jungkyukim549 <jgkim@realsn.com>
2025-04-23 09:24:17 +0900

    feat: husky

--- apps/sub/next.config.ts
+++ apps/sub/next.config.ts
@@ -1,12 +1,12 @@
-import type { NextConfig } from "next";
 import path from "node:path";
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	/* config options here */
-	reactStrictMode: true,
-	output: "standalone",
-	outputFileTracingRoot: path.join(__dirname, "../../"),
-	experimental: {},
+  /* config options here */
+  reactStrictMode: true,
+  output: "standalone",
+  outputFileTracingRoot: path.join(__dirname, "../../"),
+  experimental: {},
 };
 
 export default nextConfig;
